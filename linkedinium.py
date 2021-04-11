1783ee602e568f2f3973bd4a9a9c48b38190877b
Cristiano Giuffrida <giuffrida@cs.vu.nl>
2021-04-11 16:38:22 +0200

    Check profile/group and fix some bugs

--- linkedinium.py
+++ linkedinium.py
@@ -13,6 +13,15 @@ import sys
 import re
 import unicodedata
 import params
+import time
+
+def deSymbolize(text):
+  symbol_pattern = re.compile("["
+    u"\U0000FFFF-\U0010FFFD"
+    "]+", flags=re.UNICODE)
+
+  text = symbol_pattern.sub(r'', text)
+  return text
 
 def LIProfileURLFromNameViaGoogle(name, affiliation, affiliation_extra):
   linkedin = "site:linkedin.com"
@@ -25,6 +34,7 @@ def LIProfileURLFromNameViaGoogle(name, affiliation, affiliation_extra):
   return url
 
 def LIProfileURLFromNameViaLI(driver, name, affiliation, affiliation_extra):
+  name = name.replace("-", " ")
   if affiliation_extra:
     ret = LIProfileURLFromNameViaLISearch(driver, name + ' ' + affiliation + ' ' + affiliation_extra)
     if ret:
@@ -39,20 +49,22 @@ def LIProfileURLFromNameViaLISearch(driver, search_string):
   elem.clear()
   elem.send_keys(search_string)
   elem.send_keys(Keys.ENTER)
+  elem = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//button[@aria-label="People"]')))
+  elem.click()
   WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH, '//div[@class="search-results-container"]//span[text()="Edit search" or text()="Message" or text()="Connect" or text()="Follow" or text()="Pending"]')))
 
-  elems = driver.find_elements_by_xpath('//span[text()="Edit search"]')
-  if elems:
+  if len(driver.find_elements_by_xpath('//span[text()="Edit search"]')) > 0:
     print('[%s] PROFILE: None' % name)
     return None
   elems = driver.find_elements_by_xpath('//a[contains(@href, "https://www.linkedin.com/in/")]')
-  assert elems
+  assert len(elems)>0
   urls = []
   match_urls = []
   for elem in elems:
     name_elems = elem.find_elements_by_xpath('.//span[@dir="ltr"]/span')
-    if not name_elems or len(name_elems)>2:
+    if len(name_elems)!=2:
       continue
     url = elem.get_attribute('href')
     cmp_name = unicodedata.normalize('NFKD', name_elems[0].text)
@@ -94,21 +106,37 @@ def LIProfileLoad(driver, pURL):
   driver.get(pURL)
   elem = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, 'dist-value')))
-  name = re.compile('^[^\s]*\s(.*)\s.\sLinkedIn$').match(driver.title).group(1)
+  name = re.compile('^[^\sa-zA-Z]*\s*(.*)\s.\sLinkedIn$').match(driver.title).group(1)
+  name = deSymbolize(name).strip()
 
   status = "not-connected"
   if elem.text == "1st":
     status = "connected"
-  elif driver.find_elements_by_xpath('//span[text()="Pending"]'):
+  elif len(driver.find_elements_by_xpath('//span[text()="Pending"]')) > 0:
     status = "pending"
-  print('[%s] PROFILE LOAD: %s' % (name, status))
 
   return name, status
 
+def LIProfileCheck(driver, name, status, affiliation, affiliation_extra):
+  page = driver.find_element_by_tag_name("html")
+  for i in range(20):
+    page.send_keys(Keys.PAGE_DOWN)
+    time.sleep(0.1)
+  driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+  WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//section[@id="education-section"]')))
+  elems = driver.find_elements_by_xpath('//section[@id="education-section"]//*[contains(text(), "%s")]' % affiliation)
+  affiliation_match = len(elems) > 0
+  elems = driver.find_elements_by_xpath('//section[@id="education-section"]//*[contains(text(), "%s")]' % affiliation_extra)
+  affiliation_extra_match = len(elems) > 0
+  print('[%s] PROFILE CHECK: status=%s, affiliation_match=%d, affiliation_extra_match=%d' % (name, status, affiliation_match, affiliation_extra_match))
+
 def LIProfileConnect(driver, name, status, message, dry_run):
   if status == "connected":
+    print('[%s] CONNECT: Already connected' % name)
     return True
   if status == "pending":
+    print('[%s] CONNECT: Already pending' % name)
     return False
   if dry_run:
     print('[%s] CONNECT: Dry run' % name)
@@ -132,6 +160,23 @@ def LIProfileConnect(driver, name, status, message, dry_run):
 
   return False
 
+def LIIsGroupMember(driver, name, gURL):
+  url = gURL + "/members/"
+  driver.get(url)
+  elem = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//input[@aria-label="Search members"]')))
+  elem.click()
+  elem.clear()
+  elem.send_keys(name)
+  elem.send_keys(Keys.ENTER)
+  WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//*[text()="No search results" or text()="Message"]')))
+
+  isMember = len(driver.find_elements_by_xpath('//*[text()="Message"]')) > 0
+  print('[%s] IS_MEMBER: %d' % (name, isMember))
+
+  return isMember
+
 def LIGroupLoad(driver, gURL):
   driver.get(gURL)
   WebDriverWait(driver, 10).until(
@@ -150,10 +195,10 @@ def LIGroupInvite(driver, name, dry_run):
   elem.send_keys(name)
   elem = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH, '//div[text()="%s"]' % name)))
-  if driver.find_elements_by_xpath('//div[text()="Member"]'):
+  if len(driver.find_elements_by_xpath('//div[text()="Member"]')) > 0:
     print('[%s] INVITE: Member' % name)
     return
-  if driver.find_elements_by_xpath('//div[text()="Invited"]'):
+  if len(driver.find_elements_by_xpath('//div[text()="Invited"]')) > 0:
     print('[%s] INVITE: Pending' % name)
     return
   elem.click()
@@ -184,7 +229,11 @@ for name in params.names:
     continue
   
   name, status = LIProfileLoad(driver, pURL)
+  LIProfileCheck(driver, name, status, params.affiliation, params.affiliation_extra)
+  if LIIsGroupMember(driver, name, params.group):
+    continue
   
+  name, status = LIProfileLoad(driver, pURL)
   if LIProfileConnect(driver, name, status, params.connect_message, params.connect_dry_run):
     LIGroupLoad(driver, params.group)
     LIGroupInvite(driver, name, params.invite_dry_run)
